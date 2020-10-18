c7955e1d624eeeeab8888ad77a9c4911b06e27fc
CreamyCheese384 <69057421+CreamyCheese384@users.noreply.github.com>
2020-10-18 19:24:17 +0200

    Create composer.php

---
+++ composer.php
@@ -0,0 +1,30 @@
+<?php header('X-XSS-Protection: 0'); ?>
+<!DOCTYPE html>
+<html>
+
+
+	<head>
+		<title>Create trackable Urls!</title>
+		<style> textarea { width: 300px; height: 15px; }</style>
+	</head>
+	<body>
+		<div>
+			<form>
+			    <p>Your Url:</p>
+				<textarea name="url"><?=htmlspecialchars($_GET['url'], ENT_QUOTES,'UTF-8')?></textarea>
+				<br />
+				<p>Your E-Mail:</p>
+				<textarea name="who"><?=htmlspecialchars($_GET['who'], ENT_QUOTES,'UTF-8')?></textarea>
+				<br /><input type="submit" value="Create"></input>
+				<br />
+				<p>Your url: </p>
+				<?php 
+				if($_GET['url'] != '' && $_GET['who'] != ''){
+				echo "<p><a href='https://ccrddtcdn.000webhostapp.com/tracker?url=".$_GET['url'],"&who=".$_GET['who']."'>https://ccrddtcdn.000webhostapp.com/tracker?url=".$_GET['url'],"&who=".$_GET['who']."</a>";
+				echo "Consider using a url shortner (like <a href='https://ccrddtcdn.000webhostapp.com/tracker/?url=http://bit.ly&who=createpage@local'>bit.ly</a> or <a href='https://ccrddtcdn.000webhostapp.com/tracker/?url=http://cutt.ly&who=createpage@local'>cutt.ly</a>) to shorten this.</p>";
+				}
+				?>
+			</form>
+		</div>
+	</body>
+</html>
